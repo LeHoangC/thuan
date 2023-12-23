90926bc6f2b500edfe9eb057b6bd11edfe738934
LeHoangC <lehoangcuong0202@gmail.com>
2023-12-23 21:28:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './logo.svg'
+import './App.css'
+import Home from './components/Home'
+import Header from './components/Header'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Routes } from 'react-router-dom'
+import Details from './components/Details'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/datail" element={<Details />}></Route>
+            </Routes>
+        </>
+    )
 }
-
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Header from './Header'
+import { Link } from 'react-router-dom'
+
+function Home() {
+    const [seafood, setSeafood] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/seafood').then((res) => {
+            setSeafood(res.data)
+        })
+    }, [])
+    return (
+        <>
+            <Header />
+            <h1 className="text-center mb-3">ReactJS - hai san thay cuong</h1>
+            <div className="row" style={{ padding: '0 200px' }}></div>
+            {seafood.map((item) => {
+                return (
+                    <>
+                        <Card className="col-4 mb-2">
+                            <Card.Img variant="top" src={item.imgUrl} height="300px" width="100px" />
+                            <Card.Body>
+                                <Card.Title>{item.productName}</Card.Title>
+                                <Card.Text>{item.price}</Card.Text>
+                                <Card.Text>{item.description}</Card.Text>
+                                <Button variant="primary">
+                                    <Link to="/detail" style={{ textDecoration: 'none', color: 'white' }}>
+                                        Đặt hàng
+                                    </Link>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </>
+                )
+            })}
+        </>
+    )
+}
+export default Home
